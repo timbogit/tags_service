3e33dcac9c27788919ddd18a281ae45ac11c1beb
Tim Schmelmer <tim.schmelmer@gmail.com>
2014-03-25 22:54:56 -0600

    Adding simple 'item_id' filter on Tags#index

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -11,9 +11,10 @@ class TagsController < ApplicationController
     expires_in caching_time, public: true
   end
 
-  # List all tags
+  # List all tags (can be filtered by "item_id" parameter)
   def index
-    all_tags = Tag.all
+    item_id = params[:item_id].to_i
+    all_tags = item_id ? Tag.joins(:tagged_items).where(tagged_items: {item_id: item_id}) : Tag.all
     return json_response([]) unless newest_tag = all_tags.sort_by(&:updated_at).first
     Rails.logger.info "newest_tag is #{newest_tag.inspect}"
     render_if_stale(all_tags, last_modified: newest_tag.updated_at.utc, etag: newest_tag) do |tag_presenters|
