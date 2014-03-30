c562da4059d495bb56ae209ff0e7c8f8693f0173
Tim Schmelmer <tim.schmelmer@gmail.com>
2014-03-29 22:40:31 -0600

    Adding tags#update/#create/#destroy; also making sure that tagged_items are destroyed when a tag is deleted

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,7 @@
 class TagsController < ApplicationController
   # Show a single tag
+  # Example:
+  #  `curl -v -H "Content-type: application/json" 'http://localhost:3000/api/v1/tags/paranoid.json'
   def show
     not_found_with_max_age(caching_time) and return unless (@tag = Tag.find_by_name(params[:id]))
     Rails.logger.debug "Tag with name #{@tag.name} is #{@tag.inspect}"
@@ -12,6 +14,8 @@ class TagsController < ApplicationController
   end
 
   # List all tags (can be filtered by "item_id" parameter)
+  # Example:
+  #  `curl -v -H "Content-type: application/json" 'http://localhost:3000/api/v1/tags.json'`
   def index
     item_id = params[:item_id].to_i
     all_tags = (item_id > 0) ? Tag.joins(:tagged_items).where(tagged_items: {item_id: item_id}) : Tag.all
@@ -25,63 +29,48 @@ class TagsController < ApplicationController
   end
 
   # Create a new tag.
+  # Example:
+  #  `curl -v -H "Content-type: application/json" -X POST 'http://localhost:3000/api/v1/tags.json' \
+  #         -d '{"name":"android"}'`
   def create
-    # # validate the entity based on the incoming params hash
-    # entity_hash = entity_hash_class.new(params_for_entity)
-    # render(json: entity_hash.errors, status: :unprocessable_entity) and return unless entity_hash.valid?
-    # # update an existing entity
-    # if @entity = entity_class.find_by_key(key_for_entity(id_from_params))
-    #   if @entity.update_collection(remove_root_element_from_json_string(entity_class.name.downcase, request.body.read), params[:version])
-    #     Rails.logger.debug "Updated #{entity_class} is #{@entity.inspect}"
-    #     head :no_content
-    #   else
-    #     Rails.logger.error "cannot update because there were errors saving #{@entity.attributes.inspect} ... #{@entity.errors.to_hash}"
-    #     render(json: @entity.errors, status: :unprocessable_entity) and return
-    #   end
-    # else # create a new entity
-    #   @entity = entity_class.init_from_params(remove_root_element_from_json_string(entity_class.name.downcase, request.body.read), params[:version], key_for_entity(id_from_params))
-    #   if @entity.save
-    #     render text: '{"success": true}', status: :created, location: entity_path(params[:version], @entity.data_id)
-    #   else
-    #     Rails.logger.error "cannot create because there were errors saving #{@entity.attributes.inspect} ... #{@entity.errors.to_hash}"
-    #     render(json: @entity.errors, status: :unprocessable_entity) and return
-    #   end
-    # end
+    tag = Tag.find_or_initialize_by(params.slice(:name))
+    render(json: {error: "Tag with name #{params[:name]} already exists."}, status: :conflict) and return unless tag.new_record?
+    if tag.save
+      render text: '{"success": true}', status: :created, location: tag_path(params[:version], tag.name)
+    else
+      Rails.logger.error "cannot create because there were errors saving #{tag.attributes.inspect} ... #{tag.errors.to_hash}"
+      render(json: tag.errors, status: :unprocessable_entity)
+    end
   end
 
   # Update an existing tag.
+  # Example:
+  #  `curl -v -H "Content-type: application/json" -X PUT 'http://localhost:3000/api/v1/tags/android.json' \
+  #         -d '{"name":"paranoid"}'`
   def update
-    # TODO
+    tag = Tag.find_by_name(params[:id])
+    render(json: {error: "Tag with name #{params[:id]} does not exists."}, status: :not_found) and return if tag.nil?
+    tag.name = params[:name]
+    if tag.save
+      render text: '{"success": true}', status: :no_content, location: tag_path(params[:version], tag.name)
+    else
+      Rails.logger.error "cannot create because there were errors saving #{tag.attributes.inspect} ... #{tag.errors.to_hash}"
+      render(json: tag.errors, status: :unprocessable_entity)
+    end
   end
 
-  # Delete an tag
+  # Delete a tag
+  # Example:
+  #  `curl -v -H "Content-type: application/json" -X DELETE 'http://localhost:3000/api/v1/tags/paranoid.json'`
   def destroy
-    # TODO
+    tag = Tag.find_by_name(params[:id])
+    render(json: {error: "Tag with name #{params[:id]} does not exists."}, status: :not_found) and return if tag.nil?
+    if tag.destroy
+      render text: '{"success": true}', status: :no_content, location: tag_path(params[:version], tag.name)
+    else
+      Rails.logger.error "cannot destroy tag because there were errors deleting the tag #{tag.attributes.inspect} ... #{tag.errors.to_hash}"
+      render(json: tag.errors, status: :bad_request)
+    end
   end
-
-  # private
-
-  # # Extract the object's upstream (e.g. city ID or deal ID) from the data hash
-  # # parameter
-  # def id_from_params
-  #   params_for_entity[:id]
-  # end
-
-  # # Generates an entity key given an upstream ID or raises ApplicationController::BadParameters
-  # def key_for_entity(id)
-  #   entity_class.key_for_id(id)
-  # rescue ArgumentError, TypeError
-  #   raise ApplicationController::BadParameters.new("'id' parameter needs to be a positive integer")
-  # end
-
-  # # Extracts the data hash for the created entity from the request parameters
-  # def params_for_entity
-  #   params[entity_class.name.downcase.to_sym]
-  # end
-
-  # # Generate the URL for the specified API version and object ID
-  # def entity_path(version, id)
-  #   send "#{entity_class.name.downcase}_path", version, id
-  # end
 
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ActiveRecord::Base
   attr_accessible :name
 
-  has_many :tagged_items
+  has_many :tagged_items, dependent: :destroy
   validates_presence_of :name
   validates_uniqueness_of :name
 end
